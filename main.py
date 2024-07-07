956a301d842107e744f4f708fc4c83a912a9e9a4
ashukapasu <asutoshkumarchouhan@gmail.com>
2024-07-08 03:59:52 +0530

    latest working

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# python3 -m venv myenv
+# source myenv/bin/activate
 import serial
 import time
 import os
@@ -18,5 +20,7 @@ while True:
     print("Flag : ", flag)
     if(flag == 1):
         print("Received Command")
-        os.system('python3 detect_short_magneto.py')
+        # os.system('python3 detect_short_magneto.py')
+        os.system('/home/ashutosh/Downloads/R2_FILES/ball_detection/myenv/bin/python /home/ashutosh/Downloads/R2_FILES/ball_detection/detect_short_magneto.py')
+
     time.sleep(1)
